0960a43aa7cd471058e94dbc734f0f4aa5d08b93
Isaac <anjeeisaac@gmail.com>
2015-04-01 22:34:34 -0300

    Delete fishtest_true.sh

--- fishtest_true.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-exec curl --silent http://tests.stockfishchess.org/tests | grep 'Active' -A3 | sed 's/<h3>//' | tr '\n' ' '| sed -r 's/[^0-9 .]//g; s/ +/ /g; s/^ //' |sed '$a\' ->>/home/isaac/Documents/fishteststuff/fishtest.txt;
-
